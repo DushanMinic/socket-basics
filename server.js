99fe128b4cc319f85abc86778896b81a6bd1fb59
DusanMinic <dushanminic@gmail.com>
2016-03-16 12:27:11 +0100

    Add front-end form to submit message

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
 	console.log('User connected via socket.io!');
 
-	socket.on('message', function(message) {
+	socket.on('message', function (message) {
 		console.log('Message received: ' + message.text);
 
 		socket.broadcast.emit('message', message);
