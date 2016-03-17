706c32a42ad19bf5ec51a6722cef5ab5b65e5f8d
DusanMinic <dushanminic@gmail.com>
2016-03-17 15:25:39 +0100

    Add custom @onlineK command

--- server.js
+++ server.js
@@ -9,9 +9,36 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers (socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	if (typeof info === 'undefined') {
+		return;
+	}
+
+	Object.keys(clientInfo).forEach(function (socketId) {
+		var userInfo = clientInfo[socketId];
+
+		if (info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Online korisnici: ' + '<strong>' + users.join(', ') + '</strong>',
+		timestamp: moment().valueOf()
+	});
+
+}
+
+
+
 io.on('connection', function (socket) {
 	console.log('Korisnik konektovan kroz socket.io!');
-
 
 
 
@@ -45,8 +72,14 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Primljena poruka: ' + message.text);
 
-		message.timestamp = moment().valueOf();
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if (message.text === '@onlineK') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = moment().valueOf();
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
+
+		
 	});
 
 	//timestamp property - JavaScript timestamp(miliseconds), valueOf
